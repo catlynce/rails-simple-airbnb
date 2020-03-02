2884df2eaa3aaf5d3d856ed2360af5629b903600
Nuno Martins <nuno@alvesmartins.eu>
2020-03-02 23:22:17 +0000

    Add flat model, controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Remove all flats"
+Flat.destroy_all
+
+puts "Adding flats..."
+9.times do
+    Flat.create!(
+        name: Faker::Restaurant.name,
+        address: Faker::Address.full_address,
+        description: Faker::Restaurant.description,
+        price_per_night: Faker::Number.within(range: 45..999),
+        number_of_guests: Faker::Number.between(from: 1, to: 8)
+    )
+end
+
+puts "Done"
